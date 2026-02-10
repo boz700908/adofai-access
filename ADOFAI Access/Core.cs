e86b20cbe5852b61b66e5b8f2887307bb4fe2e07
Molitvan <82037155+Molitvan@users.noreply.github.com>
2026-02-10 18:30:08 +0100

    Add license and basic boilerplate

---
+++ ADOFAI Access/Core.cs
@@ -0,0 +1,22 @@
+﻿using MelonLoader;
+using DavyKager;
+
+[assembly: MelonInfo(typeof(ADOFAI_Access.Core), "ADOFAI Access", "1.0.0", "Molitvan", null)]
+[assembly: MelonGame("7th Beat Games", "A Dance of Fire and Ice")]
+
+namespace ADOFAI_Access
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            Tolk.Load();
+            LoggerInstance.Msg("ADOFAI Access Loaded");
+        }
+
+        public override void OnLateInitializeMelon()
+        {
+            Tolk.Output("ADOFAI Access loaded");
+        }
+    }
+}
